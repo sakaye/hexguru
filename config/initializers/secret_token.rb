fff83e5e568c5699c5f1d98326e40690a3b8e70e
Bryun Sakaye <sakaye87@gmail.com>
2013-06-15 15:31:09 -0700

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hex::Application.config.secret_token = 'b7cb816b2d1a261762283c0b894a2c97f8cd8e120787dc960058e6e400ff33cf2c01297da90d1230e4e6f5989c0e3562749e82b1b786ee53cc9ac71cd0e0d06f'
