a8f7edc935e2fe9ee14f7451147ee87b9917de38
Bryun Sakaye <sakaye87@gmail.com>
2013-06-25 21:38:19 -0700

    Added card controllers for new and edit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,11 +3,35 @@ class CardsController < ApplicationController
     @card = Card.new
   end
 
+  def create
+    @card = Card.new(params[:card])
+    if @card.save
+      flash[:notice] = "Card created"
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+
   def index
     @cards = Card.first
   end
   
   def show
     @card = Card.find(params[:id])
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update_attributes(params[:card])
+      flash[:notice] = "Card updated"
+      redirect_to @card
+    else
+      render 'edit'
+    end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -20,9 +20,8 @@
 #
 
 class Card < ActiveRecord::Base
-  attr_accessible :name, :cost, :card_type, :color, :rarity, :game_type, :attack,
-                  :defense, :trait, :game_text, :lore_text, :restriction, :set_number,
-                  :faction, :related_equipment, :image
+  attr_accessible :name, :cost, :type_ids, :color_id, :rarity_id, :game_type, :attack,
+                  :defense, :game_text, :lore_text, :restriction, :faction_id, :image
 
 
   belongs_to :color
