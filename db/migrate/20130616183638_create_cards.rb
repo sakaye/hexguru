146fa84083a8acb357e65ba4605f0943bda20c69
Bryun Sakaye <sakaye87@gmail.com>
2013-07-17 10:08:35 -0700

    fix migrations

--- db/migrate/20130616183638_create_cards.rb
+++ db/migrate/20130616183638_create_cards.rb
@@ -2,14 +2,15 @@ class CreateCards < ActiveRecord::Migration
   def up
     create_table :cards do |t|
       t.string :name
-      t.integer :cost
-      t.string :game_type
+      t.string :cost
+      t.boolean :game_type
       t.text :game_text
       t.text :lore_text
       t.boolean :restriction
       t.string :image
       t.integer :attack
       t.integer :defense
+      t.integer :color_id, :default => 1
 
       t.timestamps
     end
--- db/migrate/20130629090624_change_cost_to_string_in_cards.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCostToStringInCards < ActiveRecord::Migration
-  def up
-    change_column :cards, :cost, :string
-  end
-
-  def down
-    change_column :cards, :cost, :integer
-  end
-end
