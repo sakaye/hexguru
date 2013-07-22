5b171f32f2877295e81aad54501bca1142c3d163
Bryun Sakaye <sakaye87@gmail.com>
2013-07-22 16:01:12 -0700

    add production config options and add cap task for precompile on local

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,3 +24,15 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  namespace :assets do
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile, roles: :web, except: {no_release: true} do
+      run_locally "bundle exec rake assets:precompile"
+      find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+      end
+    end
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Hex::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.assets.initialize_on_precompile = false
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -25,7 +27,7 @@ Hex::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
