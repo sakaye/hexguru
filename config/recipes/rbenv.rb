8a4d7ffff1185e2c8dc350abe533e74e906b62cc
Bryun Sakaye <sakaye87@gmail.com>
2013-07-21 20:51:16 -0700

    update cap recipes

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -1,10 +1,9 @@
-set_default :ruby_version, "1.9.3-p125"
+set_default :ruby_version, "2.0.0-p247"
 set_default :rbenv_bootstrap, "bootstrap-ubuntu-10-04"
 
 namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
   task :install, roles: :app do
-    run "#{sudo} apt-get -y install curl git-core"
     run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
     bashrc = <<-BASHRC
 if [ -d $HOME/.rbenv ]; then 
@@ -17,7 +16,7 @@ BASHRC
     run "mv ~/.bashrc.tmp ~/.bashrc"
     run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
     run %q{eval "$(rbenv init -)"}
-    run "rbenv #{rbenv_bootstrap}"
+    # run "rbenv #{rbenv_bootstrap}"
     run "rbenv install #{ruby_version}"
     run "rbenv global #{ruby_version}"
     run "gem install bundler --no-ri --no-rdoc"
